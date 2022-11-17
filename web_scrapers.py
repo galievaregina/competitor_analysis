1ba5f6748acfd6abb710c52ab74ad38be0a361c0
galievaregina <regina-galieva-1996@mail.ru>
2022-11-17 21:52:25 +0300

    fixed mistakes timeweb,hostkey

--- web_scrapers.py
+++ web_scrapers.py
@@ -11,6 +11,7 @@ import requests
 import re
 from fake_headers import Headers
 import time
+from bs4 import BeautifulSoup
 
 current_date = date.today().strftime("%d_%m_%Y")
 
@@ -32,6 +33,7 @@ def load_hetzner(url):
     servers_hetzner = re.sub('\\n', ' ', servers_hetzner)
     servers_hetzner = json.loads(servers_hetzner)
     servers = servers_hetzner['server']
+    print(servers)
     data_hetzner = pd.DataFrame()
     for server in servers:
         key = server['key']
@@ -64,14 +66,14 @@ def load_hetzner(url):
             index=['name', 'key', 'cpu', 'cores', 'frequency',
                    'ram', 'ddr4', 'ddr3', 'hdd_size', 'ssd_size', 'nvme_size', 'price', 'setup_price', 'total_price',
                    'datacenter', 'date'])
-    data_hetzner = pd.concat([data_hetzner, row], axis=1)
+        data_hetzner = pd.concat([data_hetzner, row], axis=1)
     data_hetzner = data_hetzner.transpose()
     data_hetzner['cpu'] = data_hetzner['cpu'].str.replace(r'-', ' ')
     data_hetzner['cpu'] = data_hetzner['cpu'].str.replace(r'".*"', ' ')
     data_hetzner['cpu'] = data_hetzner['cpu'].str.replace(r'[®™]', '')
     data_hetzner['cpu'] = data_hetzner['cpu'].str.replace(r'(AMD|Intel)', '')
     data_hetzner['datacenter'] = data_hetzner['datacenter'].str.replace(r' ;', '')
-    data_hetzner.to_csv(r'C:\Users\galieva.r\PycharmProjects\competitors_analyzing\hetzner{current_date}.csv',
+    data_hetzner.to_csv(fr'D:\competitor_analysis\hetzner\{current_date}.csv',
                         index=False)
 
 
@@ -146,82 +148,6 @@ def load_servers_ru(url):
         *data_servers_ru['disks'].apply(unpack_disks))
     data_servers_ru = data_servers_ru[
         ['competitor', 'id', 'cpu_name', 'cpu_count', 'gpu_count', 'cores', 'frequency', 'ram', 'ddr4', 'ddr3',
-         'hdd_size', 'ssd_size', 'nvme_size', 'datacenter', 'price', 'date']]
-    data_servers_ru = data_servers_ru.astype(
-        {'id': int, 'cpu_name': str, 'cpu_count': int, 'gpu_count': str, 'cores': int, 'frequency': float, 'ram': int,
-         'ddr4': int, 'ddr3': int, 'hdd_size': int, 'ssd_size': int, 'nvme_size': int, 'price': float,
-         'datacenter': str})
-    data_servers_ru.to_csv(r'C:\Users\galieva.r\PycharmProjects\competitors_analyzing\servers_ru{current_date}.csv',
-                           index=False)
-
-
-def load_servers_ru(url):
-    servers_ru = load_data(url).json()
-    servers_ru = servers_ru['data']
-    data_servers_ru = pd.DataFrame()
-    counter = 0
-
-    for server in servers_ru:
-        id = server['id']
-        cpu_name = server['cpu_name']
-        cpu_count = server['processor_count']
-        gpu_count = server['gpu_count']
-        cores = server['cores']
-        freq = server['processor_speed'] / 1000
-        ram = server['ram_size']
-        if "DDR4" == server['ram_type']:
-            ddr4 = 1
-            ddr3 = 0
-        else:
-            ddr4 = 0
-            ddr3 = 1
-        disks = server['hdds_description']
-        datacenter = server['location_name'].split(' ')[0]
-        price = server['prices']['full']['hosting']['total']
-        date = current_date
-
-        config_row = [id, cpu_name, cpu_count, gpu_count, cores, freq, ram, ddr4, ddr3, disks, datacenter, price, date]
-        config_row = pd.Series(config_row, index=['id', 'cpu_name', 'cpu_count', 'gpu_count', 'cores', 'frequency',
-                                                  'ram', 'ddr4', 'ddr3', 'disks', 'datacenter', 'price', 'date'],
-                               name=counter)
-
-        data_servers_ru = pd.concat([data_servers_ru, config_row], axis=1, sort=False)
-        counter += 1
-
-    data_servers_ru = data_servers_ru.transpose()
-    data_servers_ru['cpu_name'] = data_servers_ru['cpu_name'].str.replace(r'Intel ', '')
-    data_servers_ru['disks'] = data_servers_ru['disks'].str.lower()
-    data_servers_ru['disks'] = data_servers_ru['disks'].str.replace(r'\"', '')
-    data_servers_ru['disks'] = data_servers_ru['disks'].str.replace(r'sata', 'hdd')
-    data_servers_ru['disks'] = data_servers_ru['disks'].str.replace(r'sas', 'hdd')
-
-    def unpack_disks(disks):
-        output = {
-            'hdd': 0,
-            'ssd': 0,
-            'nvme': 0
-        }
-        disks = disks.split(',')
-        for disk in disks:
-            disk = re.findall(r'(\d+ x \d+|\d+) (gb|tb) (ssd|hdd|nvme)', disk)[0]
-            if disk[1] == 'tb':
-                size = re.findall(r'(\d+)', disk[0])
-                if len(size) == 1:
-                    output[disk[2]] = output[disk[2]] + int(size[0]) * 1000
-                else:
-                    output[disk[2]] = output[disk[2]] + int(size[0]) * int(size[1]) * 1000
-            else:
-                size = re.findall(r'(\d+)', disk[0])
-                if len(size) == 1:
-                    output[disk[2]] = output[disk[2]] + int(size[0])
-                else:
-                    output[disk[2]] = output[disk[2]] + int(size[0]) * int(size[1])
-        return output['hdd'], output['ssd'], output['nvme']
-
-    data_servers_ru['hdd_size'], data_servers_ru['ssd_size'], data_servers_ru['nvme_size'] = zip(
-        *data_servers_ru['disks'].apply(unpack_disks))
-    data_servers_ru = data_servers_ru[
-        ['id', 'cpu_name', 'cpu_count', 'gpu_count', 'cores', 'frequency', 'ram', 'ddr4', 'ddr3',
          'hdd_size', 'ssd_size', 'nvme_size', 'datacenter', 'price', 'date']]
     data_servers_ru = data_servers_ru.astype(
         {'id': int, 'cpu_name': str, 'cpu_count': int, 'gpu_count': str, 'cores': int, 'frequency': float, 'ram': int,
@@ -382,9 +308,89 @@ def load_timeweb(url):
                                 index=False)
 
 
+def load_reg_ru():
+    r = requests.get('https://www.reg.ru/dedicated/')
+    soup = BeautifulSoup(r.content, "html.parser")
+    servers = soup.findAll('div', {'class': 'b-dedicated-servers-list-item'})
+    data_reg_ru = pd.DataFrame()
+    counter = 0
+
+    for server in servers:
+        proc = server.find('span', class_='b-dedicated-servers-list-item__title').get_text().strip()
+        cores = server.find('span', class_='b-dedicated-servers-list-item__subtitle').get_text().strip()
+        ram = server.find('div', class_='b-dedicated-servers-list-item__ram').get_text().strip()
+        disks = server.find('div', class_='b-dedicated-servers-list-item__hdd').decode_contents()
+        if '<br/>' in str(disks):
+            disks = str(disks)
+            disks = re.sub('<br/>', ' | ', disks)
+            disks = re.sub('<.*?>', '', disks)
+            disks = disks.strip()
+        else:
+            #         disks = disks.get_text()
+            disks = disks.strip()
+        price = server.find('div', class_='b-dedicated-servers-list-item__current-price')
+
+        # price = price.span.get_text()
+
+        config_row = [proc, cores, ram, disks, price]
+        config_row = pd.Series(config_row, index=['proc', 'cores', 'ram', 'disks', 'price'], name=counter)
+
+        data_reg_ru = pd.concat([data_reg_ru, config_row], axis=1, sort=False)
+        counter += 1
+
+    data_reg_ru = data_reg_ru.transpose()
+
+    data_reg_ru['price'] = data_reg_ru['price'].str.replace(r'[^\d]', '')
+    data_reg_ru['cores'] = data_reg_ru['cores'].str.extract(r'(\d+) ')
+    data_reg_ru['frequency'] = data_reg_ru['proc'].str.extract(r'(\d\.\d+) ГГц').astype(float)
+    data_reg_ru['ram'] = data_reg_ru['ram'].str.extract(r'(\d+) ')
+    data_reg_ru['proc'] = data_reg_ru['proc'].str.replace(r'\d\.\d+ ГГц', '')
+    data_reg_ru['proc'] = data_reg_ru['proc'].str.replace(r'(\d+) [xх×]', r'\1x')
+    data_reg_ru['disks'] = data_reg_ru['disks'].str.lower()
+    data_reg_ru['disks'] = data_reg_ru['disks'].str.replace(r'(sas|sata)', 'hdd')
+
+    def unpack_disks(disks):
+        output = {
+            'hdd': 0,
+            'ssd': 0,
+            'nvme': 0
+        }
+        disks = re.findall(r'(\d+) x (\d+\.\d+|\d+) (тб|гб) (ssd|hdd|nvme)', disks)
+        for disk in disks:
+            if disk[2] == "тб":
+                size = 1000 * int(disk[0]) * float(disk[1])
+                output[disk[3]] = output[disk[3]] + size
+            else:
+                size = int(disk[0]) * float(disk[1])
+                output[disk[3]] = output[disk[3]] + size
+        return output['hdd'], output['ssd'], output['nvme']
+
+    data_reg_ru['hdd_size'], data_reg_ru['ssd_size'], data_reg_ru['nvme_size'] = zip(
+        *data_reg_ru['disks'].apply(unpack_disks))
+    data_reg_ru['competitor'] = 'reg_ru'
+    data_reg_ru = data_reg_ru.rename({
+        'proc': 'cpu_name',
+        'cores': 'total_cores',
+        'ram': 'ram_size'
+    }, axis=1)
+    data_reg_ru = data_reg_ru[['competitor', 'price', 'cpu_name', 'frequency', 'total_cores',
+                               'ram_size', 'hdd_size', 'ssd_size', 'nvme_size']]
+    data_reg_ru['competitor'] = data_reg_ru['competitor'].astype(str)
+    data_reg_ru['price'] = data_reg_ru['price'].astype(float)
+    data_reg_ru['cpu_name'] = data_reg_ru['cpu_name'].astype(str)
+    data_reg_ru['frequency'] = data_reg_ru['frequency'].astype(float)
+    data_reg_ru['total_cores'] = data_reg_ru['total_cores'].astype(int)
+    data_reg_ru['ram_size'] = data_reg_ru['ram_size'].astype(int)
+    data_reg_ru['hdd_size'] = data_reg_ru['hdd_size'].astype(int)
+    data_reg_ru['ssd_size'] = data_reg_ru['ssd_size'].astype(int)
+    data_reg_ru['nvme_size'] = data_reg_ru['nvme_size'].astype(int)
+    data_reg_ru.to_csv(fr'D:\competitor_analysis\reg_ru\{current_date}.csv',
+                       index=False)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    url_hetzner = 'https://www.hetzner.com/_resources/app/jsondata/live_data_en.json?m=1668497923041'
+    url_hetzner = "https://www.hetzner.com/_resources/app/jsondata/live_data_en.json?m=1668706433366"
     url_timeweb = 'https://timeweb.cloud/v1.1/registration/servers'
     url_servers_ru = 'https://marketing-api.servers.ru/server_models?ru_site=true&gpu_option=0'
     url_hostkey = [
@@ -393,7 +399,8 @@ if __name__ == '__main__':
         'https://api.hostkey.com/v1/inv-api/get-presets-list?tag=bm&netag=web_noru,web_nosite&location=RU&currency=rub&pricerate=1&currencycon=br&servertype=1&filter=no&language=ru&invapi=yes']
     # load_hetzner(url_hetzner)
     # load_hostkey(url_hostkey)
-    load_timeweb(url_timeweb)
+    # load_timeweb(url_timeweb)
     # load_servers_ru(url_servers_ru)
+    load_reg_ru()
     # print(pd.read_csv(fr'D:\competitor_analysis\hostkey\{current_date}.csv'))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
